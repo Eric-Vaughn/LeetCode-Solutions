42914815eed769e6599a881ae70b2a804e97d6e6
Eric-Vaughn <vaughneric4@gmail.com>
2025-02-09 22:50:47 -0600

    Finished add-two-numbers
    Finished a recursive-less version of add-two-numbers.

--- 2-Add Two Numbers/add-two-numbers.cpp
+++ 2-Add Two Numbers/add-two-numbers.cpp
@@ -6,34 +6,56 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+
+/*
+carry_amount() figures out if we should carry nothing, a 1, or a 2 during addition
+@param sum: the sum of potienally 2 digits
+@return: the amount to be carried to the next sum
+*/
+bool should_carry(unsigned int sum) {
+    // We are returning only a 0 or 1, so why not make it a boolean value!
+    if (sum >= 10) { return true;  }
+    else           { return false; }
+}
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         unsigned int sum = l1->val + l2->val;
-        bool carry = false;
+        bool need_to_carry = false;
 
-        // ----------------------------- TO-DO: IMPLEMENT THE CARRY FUNCTIONALITY ----------------------------- 
-        if (sum >= 10) {
-            carry = true;
-            sum = sum % 10;
-        }
+        need_to_carry = should_carry(sum);
 
-        ListNode* result = new ListNode(l1->val + l2->val); // Both numbers will have at least a first digit
+        sum = sum % 10;
+
+        ListNode* result = new ListNode(sum); // Both numbers will have at least a first digit
         ListNode* result_head = result;
 
         // While both of our next digits exits
         while(l1->next && l2->next) {
-            result->next = new ListNode(l1->next->val + l2->next->val);
+            // The static_casting from boolean to unsigned int is implicit, but this way is more robust
+            sum = (l1->next->val + l2->next->val + static_cast<unsigned int>(need_to_carry));
+
+            need_to_carry = should_carry(sum);
+
+            sum = sum % 10;
+
+            result->next = new ListNode(sum);
 
             result = result->next;
             l1 = l1->next;
             l2 = l2->next;
         }
-        
         // If l1 runs out of digits, then we will just appened the rest of l2's digits
         if (!l1->next) {
             while(l2->next) {
-                result->next = new ListNode(l2->next->val);
+                sum = l2->next->val + static_cast<unsigned int>(need_to_carry);
+
+                need_to_carry = should_carry(sum); // This will always be either 0 or 1
+
+                sum = sum % 10;
+
+                result->next = new ListNode(sum);
 
                 result = result->next;
                 l2 = l2->next;
@@ -42,14 +64,21 @@ public:
         // If l2 runs out of digits, then we will just appened the rest of l1's digits
         else if (!l2->next) {
             while(l1->next) {
-                result->next = new ListNode(l1->next->val);
+                sum = l1->next->val + static_cast<unsigned int>(need_to_carry);
+
+                need_to_carry = should_carry(sum); // This will always be either 0 or 1
+
+                sum = sum % 10;
+
+                result->next = new ListNode(sum);
 
                 result = result->next;
                 l1 = l1->next;
             }
         }
 
-        // ----------------------------- DELETE ANYTHING THAT NEEDS TO BE CLEANED UP ----------------------------- 
+        if (need_to_carry) { result->next = new ListNode(static_cast<unsigned int>(need_to_carry)); }
+
         return result_head;
     }
 };
