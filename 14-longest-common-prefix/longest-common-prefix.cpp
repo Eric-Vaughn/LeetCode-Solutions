4daaf9f85609338baa10141c305bac1dd3f1efd5
oraclemoss <vaughneric4@gmail.com>
2026-09-17 16:48:55 -0500

    create 14-longest-common-prefix.cpp & it's PROMPT.txt

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// All code above (includes & namespace) is to get rid of errors + follow LeetCode's desired input
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        
+    }
+};
