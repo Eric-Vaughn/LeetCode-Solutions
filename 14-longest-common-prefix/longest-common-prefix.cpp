89c4f3d15cf06fcb9d1c6e7ac36d927509777f0b
oraclemoss <vaughneric4@gmail.com>
2026-09-17 17:05:41 -0500

    add vars to solution + std::sort() the string vec

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,13 +1,30 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
-using namespace std;
+// All code above (includes) is to get rid of errors + follow LeetCode's desired input
 
-// All code above (includes & namespace) is to get rid of errors + follow LeetCode's desired input
+/*
+// Given Code
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
         
+    }
+};
+
+*/
+
+class Solution {
+public:
+    std::string longestCommonPrefix(const std::vector<std::string>& strs) {
+        std::string commonPrefix = "";
+        std::string first = strs[0], last = strs[strs.size() - 1];
+        
+        std::sort(strs.begin(), strs.end()); // Sort strings
+
+
+        return commonPrefix;
     }
 };
